ef7a153ca32f2bb77ba70d4356f5af494d8f6899
JoanR <94060489+JRossello03@users.noreply.github.com>
2022-12-27 17:52:46 +0100

    Primera version
    Una primera version del programa.Estructura basica para empezar.

---
+++ test/SortsTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortsTest {
+
+    @Test
+    void inverteixarray() {
+
+    }
+
+    @Test
+    void bubbleSort() {
+    }
+
+    @Test
+    void insertionSort() {
+    }
+
+    @Test
+    void selectionSort() {
+    }
+}
